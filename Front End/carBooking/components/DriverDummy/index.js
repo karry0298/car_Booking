b085a281763b5d7671a0be30354f50317b3b54c3
Ashley Lobo <ashleyl@rediffmail.com>
2019-03-23 00:27:08 +0530

    Rudimentary work done. Have to Improve on it

--- Front End/carBooking/components/DriverDummy/index.js
+++ Front End/carBooking/components/DriverDummy/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-
+import { View, Text ,StyleSheet} from 'react-native';
+import AwesomeAlert from 'react-native-awesome-alerts';
+import { Button } from 'native-base';
 export default class DriverDummy extends Component {  //rename ur calss same as ur folder path
   static navigationOptions = {
     title: 'Driver Page',
@@ -9,14 +10,75 @@ export default class DriverDummy extends Component {  //rename ur calss same as 
   constructor(props) {
     super(props);
     this.state = {
+      showAlert: true,
+      onlineStatus:false,
     };
   }
 
+
+  showAlert = () => {
+    this.setState({
+      showAlert: true
+    });
+  };
+ 
+  hideAlert = () => {
+    this.setState({
+      showAlert: false
+    });
+  };
+
   render() {
+    const {showAlert} = this.state;
     return (
-      <View>
-        <Text> textInComponent </Text>
+
+      <View style={styles.container}>
+ 
+        {this.state.onlineStatus ? <Text> You are Online</Text>: <Text> You are Online</Text> }
+
+
+          
+ 
+        <AwesomeAlert
+          show={showAlert}
+          showProgress={false}
+          title="Booking needed"
+          message=" Booking needed at 100"
+          closeOnTouchOutside={true}
+          closeOnHardwareBackPress={false}
+          showCancelButton={true}
+          showConfirmButton={true}
+          cancelText="No, cancel"
+          confirmText="Yes, select"
+          confirmButtonColor="#DD6B55"
+          onCancelPressed={() => {
+            this.hideAlert();
+          }}
+          onConfirmPressed={() => {
+            this.hideAlert();
+          }}
+        />
       </View>
+      
     );
   }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  button: {
+    margin: 10,
+    paddingHorizontal: 10,
+    paddingVertical: 7,
+    borderRadius: 5,
+    backgroundColor: "#AEDEF4",
+  },
+  text: {
+    color: '#fff',
+    fontSize: 15
+  }
+});
