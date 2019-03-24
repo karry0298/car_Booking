054cf7cd5f6de7de6d8b78c5a0966a3989b25c80
Ashley Lobo <ashleyl@rediffmail.com>
2019-03-24 09:58:37 +0530

    Work

--- Front End/carBooking/components/DriverDummy/index.js
+++ Front End/carBooking/components/DriverDummy/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text ,StyleSheet} from 'react-native';
+import { View, Text ,StyleSheet,Image} from 'react-native';
+import { Container, Header, Button, Icon, Fab } from 'native-base';
 import AwesomeAlert from 'react-native-awesome-alerts';
-import { Button } from 'native-base';
 export default class DriverDummy extends Component {  //rename ur calss same as ur folder path
   static navigationOptions = {
     title: 'Driver Page',
@@ -32,24 +32,40 @@ export default class DriverDummy extends Component {  //rename ur calss same as 
     const {showAlert} = this.state;
     return (
 
-      <View style={styles.container}>
- 
-        {this.state.onlineStatus ? <Text> You are Online</Text>: <Text> You are Online</Text> }
+      <View style={{ flex: 1,alignItems:'center',flexDirection:'row' }}>
+      <View style={{ flex: 1,alignItems:'center',flexDirection:'column' }}>
+      {this.state.onlineStatus ? <Image style={{width:210, height: 200}} source={require('./img1.jpeg')} />: <Image style={{width: 220, height: 220}} source={require('./img2.jpeg')} />}
+        <View  >
+        {this.state.onlineStatus ? <Text style={{ fontWeight:'900',fontSize:50,color:'#840a0a',paddingRight:17}}>  Online</Text>: <Text style={{ fontWeight:'900',fontSize:50,color:'#840a0a',paddingRight:1}}>Offline</Text> }
+        </View>
+      </View>
 
+
+        
+        <Fab
+        active={this.state.active}
+        direction="up"
+        containerStyle={{ }}
+        style={{ backgroundColor: '#5067FF' }}
+        position="bottomRight" 
+         onPress={ () => this.setState({ onlineStatus: !this.state.onlineStatus })}
+        >
+        <Icon type="FontAwesome" name="toggle-on"  />
+      </Fab>
 
           
- 
+
         <AwesomeAlert
-          show={showAlert}
+          show={this.state.onlineStatus? showAlert:false}
           showProgress={false}
           title="Booking needed"
-          message=" Booking needed at 100"
+          message=" Booking needed at 100 "
           closeOnTouchOutside={true}
           closeOnHardwareBackPress={false}
           showCancelButton={true}
           showConfirmButton={true}
-          cancelText="No, cancel"
-          confirmText="Yes, select"
+          cancelText="Reject"
+          confirmText="Accept"
           confirmButtonColor="#DD6B55"
           onCancelPressed={() => {
             this.hideAlert();
